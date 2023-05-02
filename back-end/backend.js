4609a28d17f542fb1cffeb45edb6635baaa6ef2e
andylimm0606 <andylimm0606@gmail.com>
2023-05-01 20:15:12 -0700

    working setup

--- back-end/backend.js
+++ back-end/backend.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const cors = require('cors');
-
-const userServices = require('./models/user-services');
-
 const app = express();
 const port = 8000;
 
+const cors = require('cors');
+const userServices = require('./models/user-services');
+
 app.use(cors());
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.get('/users', async (req, res) => {
@@ -24,6 +24,8 @@ app.get('/users', async (req, res) => {
         res.status(500).send('An error ocurred in the server.');
     }
 });
+
+
 
 app.get('/users/:id', async (req, res) => {
     const id = req.params['id'];
@@ -44,14 +46,21 @@ app.post('/users', async (req, res) => {
         res.status(500).end();
 });
 
-app.delete('/users/:id', async(req, res) => {
-    const id = req.params['id']
-    console.log(id)
-    const result = await userServices.deleteUserById(id)
-    console.log(result)
-    res.status(204).end();
-})
+//use Mongo db to replace below :
+
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const id = req.params['id'];
+        await userServices.deleteUser(id);
+        res.status(204).end();
+    } catch(error){
+        console.log(error)
+        res.status(404).send('Resource not found.');
+    }
+
+});
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`Example app listening at http://localhost:${port}`);
+});   
