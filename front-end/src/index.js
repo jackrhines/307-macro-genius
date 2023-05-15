9041f89fd42ebd2456b355d3c3077acf02a995dc
AryanKumar2002 <akumar54@calpoly.edu>
2023-05-14 20:44:06 -0700

    updated index.js to route to /profile

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -8,13 +8,28 @@ import CalculatePage from './pages/CalculatePage';
 import LoginPage from './pages/Login';
 import SignUpPage from './pages/Signup';
 import Food from './pages/Food';
+import BackButton from './pages/Back';
+import GoalList from './pages/GoalList';
+import ProfileCard from './pages/ProfileCard';
+import './App.css'
 
 
+const profile = {
+  name: 'Steph Curry',
+  height: 188,
+  weight: 185,
+  age: 35,
+  activityLevel: 'Very Active',
+  imageUrl: 'https://cdn.nba.com/headshots/nba/latest/1040x760/201939.png'
+};
+
+const goals = ['Goal 1', 'Goal 2', 'Goal 3'];
+const achievements = ['Achievement 1', 'Achievement 2', 'Achievement 3'];
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <LoginPage/>,
+    element: <LoginPage/>
   },
   {
     path: "signup",
@@ -28,6 +43,15 @@ const router = createBrowserRouter([
     path: "foods",
     element: <Food/>,
   },
+  {
+    path: "profile",
+    element: <div className="profile">
+      <GoalList title="Your Goals" goals={goals} />
+      <ProfileCard profile={profile} />
+      <GoalList title="Your Achievements" goals={achievements} />
+      <BackButton />
+    </div>,
+  }
   
 ]);
 
