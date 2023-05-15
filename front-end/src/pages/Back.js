a59ab247887f644931faa745fb3d56b42071917f
AryanKumar2002 <akumar54@calpoly.edu>
2023-05-14 20:41:33 -0700

    added profile to main

---
+++ front-end/src/pages/Back.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Back.css';
+
+const BackButton = () => {
+  return (
+    <button className="back-button">
+      Back
+    </button>
+  );
+};
+
+export default BackButton;
---
+++ front-end/src/pages/GoalList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './GoalList.css';
+
+const GoalList = ({ title, goals }) => {
+  return (
+    <div className="goal-list">
+      <h2>{title}</h2>
+      <ul>
+        {goals.map((goal, index) => (
+          <li key={index}>{goal}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default GoalList;
