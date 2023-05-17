a86306288aafd3b0ee28b7ed21a60f4c059a6605
andylimm0606 <andylimm0606@gmail.com>
2023-05-17 09:05:27 -0700

    log in...

--- back-end/models/user-services.js
+++ back-end/models/user-services.js
@@ -30,21 +30,6 @@ mongoose
   .catch((error) => console.log(error));
 
 
-// Curb Cores Error by adding a header here
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
-
-
 
   async function addUser(user) {
     try {
