c516f80cd7925f9e43ed8c5f25ddf6ddcb3a9648
andylimm0606 <andylimm0606@gmail.com>
2023-05-17 09:04:24 -0700

    log in...

--- back-end/backend_with_db.js
+++ back-end/backend_with_db.js
@@ -11,6 +11,21 @@ const auth = require("./auth");
 const app = express();
 const port = 8000;
 
+
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 
