a2f36d0cade4661e717a4e98d45ac0e20f41b19e
AryanKumar2002 <akumar54@calpoly.edu>
2023-05-25 19:10:21 -0700

    added profile backend

---
+++ back-end/models/profile.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  sex: {
+    type: String,
+    required: true,
+  },
+  height: {
+    type: Number,
+    required: true,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  activityLevel: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("UserProfile", ProfileSchema);
