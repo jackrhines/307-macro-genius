734c15095080a43468e2f1d1ef9e79fa0a987c75
Ahren27 <ahren.dosanjh@gmail.com>
2023-05-26 01:37:11 -0700

    Top Labels

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -22,11 +22,11 @@ const App = () => {
             <MainRoutes/>
             <div className="calculate-page-overlap">
                 <div className="top-logo-text-wrapper">MacroGenius</div>
-                <ul className="calcuate-navigate-menu">
-                    <li><a href="/calculate">Calculate</a></li>
-                </ul>
                 <ul className="home-navigate-menu">
                     <li><a href="/landingPage">Home</a></li>
+                </ul>
+                <ul className="calcuate-navigate-menu">
+                    <li><a href="/calculate">Calculate</a></li>
                 </ul>
                 <ul className="login-navigate-menu">
                     <li><a href="/login">Login</a></li>
--- front-end/src/pages/LandingPage.js
+++ front-end/src/pages/LandingPage.js
@@ -1,15 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "./LandingPage.module.css";
 import axios from "axios";
+import Cookies from "universal-cookie";
+import endOfDay from 'date-fns/endOfDay';
+import startOfDay from 'date-fns/startOfDay';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const cookies = new Cookies();
 
 const LandingPage = (props) => {
+    // const [date, setDate] = useState(Date.now());
+    // const [currentUser, setCurrentUser] = useState(null);
+
+    const current = new Date();
+    const date = `${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`;
+
     return (
         <div className={styles.body}>
             <div className={styles.profilePicture}>
-                <circle></circle>
+                <circle className='profilePicture'/>
+            </div>
+            <div className={styles.welcomeMessage}>
+                <label>Welcome, {cookies.get("USERNAME")}!</label>
             </div>
             <div className={styles.motivationalQuote}>
                 <label>"The only bad workout is the one that didn't happen."</label>
+            </div>
+            <div className={styles.dateWrapper}>
+                <label>{date}</label>
             </div>
     </div>
     );
