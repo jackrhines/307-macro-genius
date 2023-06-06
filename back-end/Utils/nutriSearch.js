5fcb89db4f3c2e51759616bc4afde045daf8158f
andylimm0606 <andylimm0606@gmail.com>
2023-06-05 17:58:31 -0700

    ciiiii

--- back-end/Utils/nutriSearch.js
+++ back-end/Utils/nutriSearch.js
@@ -1,26 +1,26 @@
-const axios = require('axios');
-require('dotenv').config();
+const axios = require("axios");
+require("dotenv").config();
 
-async function nutriSearch(food){
-	console.log(`The food to be searched is ${food}`);
-	const message = `Get the nutrition information on ${food}, specifically only report calorie per serving and protein per serving` 
-	try {
-        const response = await axios.post(
-            "https://api.openai.com/v1/chat/completions",
-            {
-                model: "gpt-3.5-turbo",
-                messages: [
-                    { role: "system", content: "You are a helpful assistant." },
-                    { role: "user", content: message },
-                ],
-            },
-            {
-                headers: {
-                    'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-                    'Content-Type': 'application/json',
-                },
-            }
-        )
+async function nutriSearch(food) {
+  console.log(`The food to be searched is ${food}`);
+  const message = `Get the nutrition information on ${food}, specifically only report calorie per serving and protein per serving`;
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: "You are a helpful assistant." },
+          { role: "user", content: message },
+        ],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     return response.data.choices[0].message;
   } catch (error) {
